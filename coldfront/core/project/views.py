e71319596a8eba4b88bade14be248e8792ec790d
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2025-12-15 18:02:26 -0500

    project managers should be able to see allocations & add test for managers viewing allocations without being an allocationuser
    Signed-off-by: Sajid Ali <sajidsyed2021@u.northwestern.edu>
    
    	modified:   coldfront/core/project/views.py
    
    	modified:   coldfront/core/project/tests/test_views.py
    	modified:   coldfront/core/project/views.py

--- coldfront/core/project/views.py
+++ coldfront/core/project/views.py
@@ -178,12 +178,10 @@ class ProjectDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
                             ]
                         )
                         & (
-                            (
-                                Q(allocationuser__user=self.request.user)
-                                & Q(allocationuser__status__name__in=["Active", "PendingEULA"])
-                            )
-                            | Q(project__projectuser__role__name="Manager")
+                            Q(allocationuser__user=self.request.user)
+                            & Q(allocationuser__status__name__in=["Active", "PendingEULA"])
                         )
+                        | Q(project__projectuser__role__name="Manager")
                     )
                     .distinct()
                     .order_by("-end_date")
